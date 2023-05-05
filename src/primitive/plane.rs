7421732e98b816f3618b197a2705aeff2d39e2d6
Erwan Gonzales <erwan.gonzales@epitech.eu>
2023-05-05 15:42:16 +0200

    updated plane, now fits new Intersection struct

--- src/primitive/plane.rs
+++ src/primitive/plane.rs
@@ -4,7 +4,7 @@ use crate::{
     ray::Ray,
 };
 
-use super::Primitive;
+use super::{Primitive, Intersection};
 
 #[derive(Debug)]
 pub struct Plane {
@@ -28,15 +28,15 @@ impl Primitive for Plane {
         self.material
     }
 
-    fn hits(&self, ray: &Ray) -> Vec<(Vector3D, &dyn Primitive)> {
-        let mut v: Vec<(Vector3D, &dyn Primitive)> = Vec::new();
+    fn hits(&self, ray: &Ray) -> Vec<Intersection> {
+        let mut v: Vec<Intersection> = Vec::new();
         let delta = self.normal.dot(ray.direction());
         if delta.abs() < 1e-6 {
             return v;
         }
         let t = (self.origin - ray.origin()).dot(self.normal) / delta;
         if t > 0.0 {
-            v.push((ray.origin() + ray.direction() * t, self));
+            v.push(Intersection::new(ray.origin() + ray.direction() * t, self));
         }
         v
     }
