73b3c2331ba1c0fce11edbef9821dd7ae60603b0
Erwan Gonzales <erwan.gonzales@epitech.eu>
2023-05-05 15:06:00 +0200

    Whole camera redesign Holds an image whole new constructor new getters for viewport dimension image and focal lenght

--- src/camera.rs
+++ src/camera.rs
@@ -1,25 +1,58 @@
+mod image;
+pub use image::Image;
+
 use crate::{
     math::{Rectangle3D, Vector3D},
-    ray::Ray,
+    ray::Ray, material::Color,
 };
 
 #[derive(Debug)]
 pub struct Camera {
-    origin: Vector3D,
+    eye: Vector3D,
     screen: Rectangle3D,
+    focal_length: f64,
+    image: Image,
+    viewport_width: f64,
+    viewport_height: f64,
 }
 
 impl Camera {
-    pub fn new(origin: Vector3D, screen: Rectangle3D) -> Self {
-        Self { origin, screen }
+    pub fn new(eye: Vector3D, image: Image, focal_length: f64) -> Self {
+        let viewport_width = image.width() as f64;
+        let viewport_height = image.height() as f64;
+        let horizontal = Vector3D::new(viewport_width, 0., 0.);
+        let vertical = Vector3D::new(0., viewport_height, 0.);
+        let lower_left_corner = eye - horizontal / 2.0 - vertical / 2.0 - Vector3D::new(0.0, 0.0, focal_length);
+        let screen = Rectangle3D::new(lower_left_corner, horizontal + vertical);
+        Self {
+            eye,
+            screen,
+            focal_length,
+            image,
+            viewport_width,
+            viewport_height,
+        }
     }
-
-    pub fn origin(&self) -> Vector3D {
-        self.origin
+    pub fn eye(&self) -> Vector3D {
+        self.eye
     }
-
-    pub fn ray(&self, u: f64, v: f64) -> Ray {
+    pub fn at(&self, u: f64, v: f64) -> Ray {
         let p = self.screen.point_at(u, v);
-        Ray::new(self.origin, Vector3D::new(p.x(), p.y(), p.z()))
+        Ray::new(self.eye, Vector3D::new(p.x(), p.y(), p.z()))
+    }
+    pub fn write(&mut self, color: Color) {
+        self.image.write(&color);
+    }
+    pub fn viewport_height(&self) -> f64 {
+        self.viewport_height
+    }
+    pub fn viewport_width(&self) -> f64 {
+        self.viewport_width
+    }
+    pub fn image(&self) -> &Image {
+        &self.image
+    }
+    pub fn image_as_mut(&mut self) -> &mut Image {
+        &mut self.image
     }
 }
