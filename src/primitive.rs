2a0a323d91e554101216c02cf831a44b04380b4f
Erwan Gonzales <erwan.gonzales@epitech.eu>
2023-05-05 15:01:28 +0200

    implement the  intersection structure

--- src/primitive.rs
+++ src/primitive.rs
@@ -5,6 +5,27 @@ use crate::{material::Material, math::Vector3D, ray::Ray};
 
 pub trait Primitive {
     fn material(&self) -> Material;
-    fn hits(&self, ray: &Ray) -> Vec<(Vector3D, &dyn Primitive)>;
+    fn hits(&self, ray: &Ray) -> Vec<Intersection>;
     fn normal(&self, point: Vector3D) -> Vector3D;
+}
+
+#[derive(Clone, Copy)]
+pub struct Intersection<'a> {
+    point: Vector3D,
+    primitive: &'a dyn Primitive,
+}
+
+impl<'a> Intersection<'a> {
+    pub fn new(point: Vector3D, primitive: &'a dyn Primitive) -> Self {
+        Intersection { point, primitive }
+    }
+    pub fn point(&self) -> Vector3D {
+        self.point
+    }
+    pub fn primitive(&self) -> &dyn Primitive {
+        self.primitive
+    }
+    pub fn material(&self) -> Material {
+        self.primitive.material()
+    }
 }
